30ec926c40dc3959e2174ad1b940f9eb4f6f709f
shredped <>
2018-03-14 13:39:38 +0700

    / to /home

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/home', function(request, response)
+app.get('/', function(request, response)
+{
+    response.end('Hello World');
+});
+
+app.post('/', function(request, response)
 {
     response.send('Hello World');
 });
